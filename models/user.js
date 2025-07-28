691333b28c617fdcd23dba203db160553159763c
Riri_Css <rihanat52@gmail.com>
2025-07-28 17:23:22 +0100

    Update on user.js

--- models/user.js
+++ models/user.js
@@ -79,4 +79,4 @@ const userSchema = new mongoose.Schema({
   recommendedCareers: [String]
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.user || mongoose.model('User', userSchema);
