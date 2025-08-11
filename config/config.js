d5d7845166001a4066123fd1f20f6a5d62af9c1e
Riri_Css <rihanat52@gmail.com>
2025-08-11 15:17:52 +0100

    add config.js

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// This line tells our application to load the variables from your .env file.
+require('dotenv').config();
+
+// The module.exports statement makes the contents of this file available
+// to other files in your project.
+module.exports = {
+  // We've updated the property and the environment variable to use the OpenAI API key.
+  // We access the variable from the .env file using 'process.env.OPENAI_API_KEY'.
+  // The '||' part provides a fallback value if the environment variable is not defined.
+  openaiApiKey: process.env.OPENAI_API_KEY || '7991702029:AAFjSX7IymrAQCUtAv6RW9MKLflUETCunvQ',
+};
